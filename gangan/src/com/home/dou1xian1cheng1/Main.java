3758478f230fb819b8133e05e689a24a383807a0
黄威 <1724347606@qq.com>
2020-06-14 12:07:39 +0800

    我的第二个idea

--- gangan/src/com/home/dou1xian1cheng1/Main.java
+++ gangan/src/com/home/dou1xian1cheng1/Main.java
@@ -4,10 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("也请金额");
-        System.out.println("西瓯宁县");
-        System.out.println("西瓯宁县");
-        System.out.println("西瓯宁县");
-        System.out.println("西瓯宁县");
+
     }
 }
