d6025363bb0b3b0be6c5a7f4f083957792b50fe8
huangwei66 <1724347606@qq.com>
2020-07-09 22:17:19 +0800

    ..

--- gangan/src/com/home/multi-thread/network programming/Client.java
+++ gangan/src/com/home/multi-thread/network programming/Client.java
@@ -4,50 +4,20 @@ import java.util.Scanner;
 
 public class Client {
 
-
     public static void main(String[] args) throws IOException {
 
         Socket socket = new Socket("127.0.0.1", 55555);
-        System.out.println("这是客户端,连接上服务器了");
-
-
-
-
-
+        System.out.println("这是客户端,连接上服务器了,等待回应");
+        while (true) {
+            InputStream is = socket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String txt = br.readLine();
+            System.out.println(txt);
+            OutputStream os = socket.getOutputStream();
+            PrintStream ps = new PrintStream(os);
+            Scanner scanner = new Scanner(System.in);
+            String talk = scanner.next();
+            ps.println("小明的哥哥:" + talk);
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*第一次连上
-    public static void main(String[] args) throws IOException {
-
-        Socket socket = new Socket("127.0.0.1", 55555);
-        System.out.println("这是客户端,连接上服务器了");
-        InputStream is = socket.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        String txt = br.readLine();
-        System.out.println("收到: " + txt);
-
-        OutputStream os=socket.getOutputStream();
-        PrintStream ps=new PrintStream(os);
-        Scanner scanner=new Scanner(System.in);
-        String talk=scanner.next();
-        ps.println("你连上了客户端,我要告诉你"+talk);
-
-    }*/
 }
--- gangan/src/com/home/multi-thread/network programming/Server.java
+++ gangan/src/com/home/multi-thread/network programming/Server.java
@@ -4,42 +4,24 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-
-       ServerSocket server=new ServerSocket(55555);
-        System.out.println("服务器程序启动");
-
-        while (true) {
-            Socket socket = server.accept();
-            System.out.println("对方连上了");
-        }
-
-    }
-
-
-
-/*第一次连上
 
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(55555);
         System.out.println("服务器程序启动");
         Socket socket = server.accept();
         System.out.println("对方连上了,想说什么");
-        OutputStream os=socket.getOutputStream();
-        PrintStream ps=new PrintStream(os);
-        Scanner scanner=new Scanner(System.in);
-        String talk=scanner.next();
-        ps.println("我要告诉你: "+talk);
-        InputStream is = socket.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        System.out.println("我回复说:");
-        String txt = br.readLine();
-        System.out.println("收到: " + txt);
-
-        System.out.println("服务器程序关闭");
-
+        OutputStream os = socket.getOutputStream();
+        PrintStream ps = new PrintStream(os);
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String talk = scanner.next();
+            ps.println(" 北方的狼: " + talk);
+            InputStream is = socket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String txt = br.readLine();
+            System.out.println(txt);
+        }
     }
-*/
 
 
 }
