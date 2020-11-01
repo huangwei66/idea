aba9d1b490ca8e9eafe99a0393857fe7daa4aec3
windhua <1724347606@qq.com>
2020-11-01 09:56:54 +0800

    测试

--- gangan/src/com/home/dou1xian1cheng1/Main.java
+++ gangan/src/com/home/dou1xian1cheng1/Main.java
@@ -3,7 +3,9 @@ package com.home.dou1xian1cheng1;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+      System.out.println(
+          "这是测试"
+      );
 
     }
 }
