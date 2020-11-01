9a18a66b02e0a34dfb7a6c04896d6de38dae9dab
windhua <1724347606@qq.com>
2020-11-01 09:55:32 +0800

    测试

--- gangan/src/com/home/dou1xian1cheng1/Main.java
+++ gangan/src/com/home/dou1xian1cheng1/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-
+        String s = new String();
+        s="hahahaa";
+        System.out.println(s);
     }
 }
